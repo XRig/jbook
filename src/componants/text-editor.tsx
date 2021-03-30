1976a9f0205e3acd573f4c9b4ded075b5477361a
Rex Righetti <rex@zepge.com>
2021-03-30 14:31:25 -0700

    Stop on Code Editor -- Starting Redux

--- src/componants/text-editor.tsx
+++ src/componants/text-editor.tsx
@@ -2,18 +2,18 @@ import { useState, useEffect, useRef } from "react";
 import MDEditor from '@uiw/react-md-editor';
 import './text-editor.css'
 
-interface Props { }
 
-function TextEditor(props: Props) {
-    const { } = props
+
+function TextEditor() {
+
     const [editing, setEditing] = useState(false)
     const [value, setValue] = useState("# Enter Text");
-    const ref = useRef<HTMLDivElement|null>(null)
+    const ref = useRef<HTMLDivElement | null>(null)
 
     useEffect(() => {
         const listener = (event: MouseEvent) => {
-            if(!ref.current) return;
-            if(event.target && ref.current.contains(event.target as Node)) return;
+            if (!ref.current) return;
+            if (event.target && ref.current.contains(event.target as Node)) return;
             setEditing(false)
         }
         window.addEventListener('click', listener, { capture: true })
@@ -23,13 +23,19 @@ function TextEditor(props: Props) {
         }
     }, [])
     if (!editing) {
-        return <div className='text-editor' onClick={() => setEditing(true)}> <MDEditor.Markdown source={value} /> </div>
+        return (
+            <div className='text-editor card' onClick={() => setEditing(true)}>
+                <div className="card-content">
+
+                    <MDEditor.Markdown source={value} /> </div>
+            </div>
+        )
     }
     return (
         <div className="text-editor" ref={ref}>
             <MDEditor
-                value={value} 
-                onChange={(v) =>setValue(v ||'' )}/>
+                value={value}
+                onChange={(v) => setValue( v || '' )} />
         </div>
     );
 
