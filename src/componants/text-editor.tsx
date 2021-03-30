7acb7ddffea0473e00ad14cf4cc9848ed829fe86
Rex Righetti <rex@zepge.com>
2021-03-30 13:57:48 -0700

    Make MD editor expand on click

--- src/componants/text-editor.tsx
+++ src/componants/text-editor.tsx
@@ -1,12 +1,32 @@
+import { useState, useEffect} from "react";
+import MDEditor from '@uiw/react-md-editor';
 
-
-interface Props {}
+interface Props { }
 
 function TextEditor(props: Props) {
-    const {} = props
+    const { } = props
+    const [editing, setEditing] = useState(false)
+    const [value, setValue] = useState("**Hello world!!!**");
 
-    return (<div>Hi</div>)
-    
+
+    useEffect(() => {
+        const listener = () => {setEditing(false)}
+        window.addEventListener('click',listener, {capture:true})
+
+        return () => {
+            document.removeEventListener('click',listener, {capture:true})
+        }
+    },[])
+    if (!editing) {
+        return <div onClick={() => setEditing(true)}> <MDEditor.Markdown source={value} /> </div>
+    }
+    return (
+        <div className="container">
+            <MDEditor
+                value={value} />
+        </div>
+    );
+
 }
 
 export default TextEditor
