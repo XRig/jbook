25ea414bc4fabdb2166c22b4ca2edebd037f4531
Rex Righetti <rex@zepge.com>
2021-03-29 21:03:20 -0700

    Add index
    To this point, this project was built without version control.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,91 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reactDOM from 'react-dom'
+import { useEffect, useState, useRef } from 'react'
+import * as esbuild from 'esbuild-wasm'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './componants/code-editor'
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const ref = useRef<any>();
+    const iframe = useRef<any>();
+    const [input, setInput] = useState<string | undefined>('')
+
+    const startService = async () => {
+        await esbuild.initialize({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+        ref.current = true
+
+    }
+
+    useEffect(() => {
+        startService();
+    }, [])
+
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return
+        }
+        if (typeof input ==="string"){
+            const result = await esbuild.build({
+                entryPoints: ['index.js'],
+                bundle: true,
+                write: false,
+                plugins: [
+                    unpkgPathPlugin(),
+                    fetchPlugin(input)
+                ],
+                define: {
+                    'process.env.NODE_ENV': '"production"',
+                    global: 'window'
+                }
+            })
+    
+            //setCode(result.outputFiles[0].text)
+            iframe.current.srcdoc = html
+            iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+    
+        }
+        
+    }
+
+    const html = `
+    <html>
+    <head></head>
+    <body>
+        <div id="root"></div>
+        <script>
+            window.addEventListener('message', (event => {
+                try {
+                eval(event.data);
+                } catch(e) {
+                    const root = document.querySelector('#root');
+                    root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + e + '</div>'
+                    console.error(e)
+                }
+            }))
+        </script>
+    </body>
+    </html>
+        `
+
+    return <div>
+        <CodeEditor initialValue="" onChange={e => setInput(e)} />
+        
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+
+        <iframe title='preview' ref={iframe} sandbox="allow-scripts" srcDoc={html}></iframe>
+
+
+    </div>
+
+
+}
+
+reactDOM.render(<App />, document.querySelector('#root'))
